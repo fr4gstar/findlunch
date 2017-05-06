3feef126c4c1ef7dedfc7bb92a5fcee0f82e06fd
david <david.sautter@web.de>
2017-05-06 12:09:03 +0200

    begin offers

--- ionicApp/src/app/app.component.ts
+++ ionicApp/src/app/app.component.ts
@@ -6,6 +6,7 @@ import {SplashScreen} from '@ionic-native/splash-screen';
 import {HomePage} from '../pages/home/home';
 import {ListPage} from '../pages/list/list';
 import {Firebase} from "@ionic-native/firebase";
+import {OffersPage} from "../pages/Offers/offers";
 
 @Component({
     templateUrl: 'app.html'
@@ -23,7 +24,8 @@ export class MyApp {
         // used for an example of ngFor and navigation
         this.pages = [
             {title: 'Home', component: HomePage},
-            {title: 'List', component: ListPage}
+            {title: 'List', component: ListPage},
+            {title: 'Offers', component: OffersPage}
         ];
 
     }
---
+++ ionicApp/src/pages/Offers/offers.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+    selector: 'offers',
+    templateUrl: 'offers.html'
+})
+export class OffersPage {
+
+}
