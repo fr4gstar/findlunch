455f1b4a609e0445d573cf247934f5bc4e673192
DASA <dasa@muc.isoware.com>
2017-05-18 17:48:19 +0200

    showing current position

--- ionicApp/src/pages/home/home.ts
+++ ionicApp/src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import {Component} from "@angular/core";
 import {NavController, Platform} from "ionic-angular";
 import {Coordinates, Geolocation} from "@ionic-native/geolocation";
-import {GoogleMap, GoogleMaps, GoogleMapsEvent} from "@ionic-native/google-maps";
+import {CameraPosition, GoogleMap, GoogleMaps, GoogleMapsEvent, LatLng, Marker} from "@ionic-native/google-maps";
 
 export const ANDROID_API_KEY = "AIzaSyAvO9bl1Yi2hn7mkTSniv5lXaPRii1JxjI";
 
@@ -15,7 +15,9 @@ export class HomePage {
 
 
   constructor(public navCtrl: NavController, private geolocation: Geolocation, private platform: Platform, private googleMaps: GoogleMaps) {
-    this.platform.ready().then(() => this.getGeolocation())
+    this.platform.ready().then(() => {
+      // access native APIs
+    })
   }
 
   // Load map only after view is initialized
@@ -36,77 +38,25 @@ export class HomePage {
       () => {
         console.log('Map is ready!');
         // Now you can add elements to the map like the marker
+
+        this.geolocation.getCurrentPosition().then((res) => {
+          let pos = new LatLng(res.coords.latitude, res.coords.longitude);
+
+          let camPos: CameraPosition = {
+            target: pos,
+            zoom: 16
+          };
+
+          map.moveCamera(camPos);
+
+          // create current location marker
+          map.addMarker({
+            position: pos,
+          }).then((marker: Marker) => {
+            marker.showInfoWindow();
+          })
+        })
       }
     );
   }
-
-
-  private getGeolocation() {
-    this.geolocation.getCurrentPosition().then((res) => {
-      this.pos = res.coords;
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    });
-  }
-
-  sendToWeb() {
-    // Add Serverkey and Webtoken here
-    var webKey = 'SERVERKEY-API';
-    var toWeb = 'WEB-TOKEN';
-
-    let notification = {
-      'title': 'Bestellung XY eingegangen',
-      'body': 'Kunde: Maximilian Mustermann',
-      'icon': '/assets/icon/favicon.ico',
-      'click_action': 'http://localhost:8100'
-    };
-
-    fetch('https://fcm.googleapis.com/fcm/send', {
-      'method': 'POST',
-      'headers': {
-        'Authorization': 'key=' + webKey,
-        'Content-Type': 'application/json'
-      },
-      'body': JSON.stringify({
-        'notification': notification,
-        'to': toWeb
-      })
-    }).then(function(response) {
-      console.log(response);
-    }).catch(function(error) {
-      console.error(error);
-    })
-
-  }
-
-  sendToAndroid() {
-    // Add Serverkey and Android Token
-    var androidKey = 'SERVERKEY-API';
-    var toAndroid = 'ANDROID-TOKEN';
-
-    let notification = {
-      'title': 'Bestätigung der Bestellung XY',
-      'body': 'Abholbereit in ca. 20 min',
-      'icon': 'firebase-logo.png',
-      'click_action': 'http://localhost:8100'
-    };
-
-    fetch('https://fcm.googleapis.com/fcm/send', {
-      'method': 'POST',
-      'headers': {
-        'Authorization': 'key=' + androidKey,
-        'Content-Type': 'application/json'
-      },
-      'body': JSON.stringify({
-        'notification': notification,
-        'to': toAndroid
-      })
-    }).then(function(response) {
-      console.log(response);
-    }).catch(function(error) {
-      console.error(error);
-    })
-
-  }
-
 }
