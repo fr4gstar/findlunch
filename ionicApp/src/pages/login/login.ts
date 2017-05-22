2b8ba91aba0e01285265a1426224c264a790faf2
Unknown <smorandi@hm.edu>
2017-05-22 16:12:20 +0200

    Registerbutton implemented + fix

--- ionicApp/src/pages/login/login.ts
+++ ionicApp/src/pages/login/login.ts
@@ -13,13 +13,14 @@ import {HomePage} from "../home/home";
 })
 export class LoginPage {
 
+  isLoggedIn : boolean = false;
   constructor(private navCtrl: NavController, private http: Http, private toastCtrl: ToastController) {
   }
 
 
   public submitCredentials(userName: string, password: string) {
 
-    let encodedCredentials: String = btoa(userName+":"+password);
+    let encodedCredentials: String = atob(userName+":"+password);
     console.log(encodedCredentials);
     let headers = new Headers({
       'Content-Type': 'application/json',
@@ -29,6 +30,7 @@ export class LoginPage {
     let options = new RequestOptions({headers: headers});
     this.http.get(SERVER_URL + "/api/login_user", options).subscribe(
       (res) => {
+        this.isLoggedIn = true;
         const toast = this.toastCtrl.create({
           message: "Login Erfolgreich",
           duration: 3000
@@ -36,8 +38,9 @@ export class LoginPage {
         toast.present();
         this.navCtrl.push(HomePage);
       }, (err) => {
-        console.error(err)
+        alert("E-Mail und/oder Passwort nicht bekannt");
+
       })
   }
-
+  //TODO : goToRegisterPage();
 }
