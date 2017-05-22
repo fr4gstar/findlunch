7821da5fb4c413e2b4ecf1fc258713df7c19cb4d
Unknown <smorandi@hm.edu>
2017-05-22 13:05:30 +0200

    Logging in with registered Credentials works
    Pushes to homepage
    Next: show only stars for entering passwords

--- ionicApp/src/pages/login/login.ts
+++ ionicApp/src/pages/login/login.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import {NavController, ToastController} from "ionic-angular";
-import {HTTP} from "@ionic-native/http";
 import {Headers, Http, RequestOptions} from "@angular/http";
 
 import {SERVER_URL} from "../../app/app.module";
 import {HomePage} from "../home/home";
-import { ViewChild } from '@angular/core';
 
 
 @Component({
@@ -14,31 +12,14 @@ import { ViewChild } from '@angular/core';
 
 })
 export class LoginPage {
-  // private userName : String;
-  // private Password : String;
 
   constructor(private navCtrl: NavController, private http: Http, private toastCtrl: ToastController) {
   }
 
+
   public submitCredentials(userName: string, password: string) {
-    //   //TODO: find way to base64 encode username and pw for http basic auth
-    //   var authHeader: Object = this.http.getBasicAuthHeader(userName,password);
-    //   //var credentials: String = $base64.encode(userName+":"+password);
-    //
-    //   this.http.get(`${SERVER_URL}/login_user`, {}, authHeader)
-    //    .then(data =>{
-    //      this.navCtrl.push(RestaurantsPage);
-    //      const toast = this.toastCtrl.create({
-    //        message: "Login erfolgreich",
-    //        duration: 3000
-    //      });
-    //      toast.present();
-    //     })
-    //    .catch(error => {
-    //     console.log(error.error);
-    //   });
-    //  }
-    let encodedCredentials: String = atob(userName+""+password);
+
+    let encodedCredentials: String = btoa(userName+":"+password);
     console.log(encodedCredentials);
     let headers = new Headers({
       'Content-Type': 'application/json',
@@ -58,12 +39,5 @@ export class LoginPage {
         console.error(err)
       })
   }
-  public show(userName: string, password: string){
-    alert(userName+password);
-  }
 
-  public encode64(userName: string, password: string){
-    let creds = userName+password;
-    alert(btoa(creds));
-  }
 }
