18e5dd831e7abd69fa9986decbb31389eb922a4a
Sergej Bardin <sergej_bardin@yahoo.de>
2017-05-30 11:22:09 +0200

    Added spec js and ts

---
+++ ionicApp/spec/list.spec.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+describe('Component: ListPage', function () {
+    /*
+    let component: ListPage;
+  
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [ListPage]
+      });
+  
+      const fixture = TestBed.createComponent(ListPage);
+      component = fixture.componentInstance;
+    });
+  
+    it('should have a defined component', () => {
+        expect(component).toBeDefined();
+      });
+    */
+    describe('1st tests', function () {
+        it('true is true', function () { return expect(true).toBe(true); });
+    });
+});
---
+++ ionicApp/spec/offers.spec.ts
@@ -0,0 +1,32 @@
+import { OffersPage } from '../src/pages/offers/offers';
+import {} from 'jasmine';
+
+import {
+  TestBed,
+  inject
+} from '@angular/core/testing';
+
+describe('Component: OffersPage', () => {
+  /*
+   let component: ListPage;
+
+   beforeEach(() => {
+   TestBed.configureTestingModule({
+   declarations: [ListPage]
+   });
+
+   const fixture = TestBed.createComponent(ListPage);
+   component = fixture.componentInstance;
+   });
+
+   it('should have a defined component', () => {
+   expect(component).toBeDefined();
+   });
+   */
+
+  describe('1st tests', () => {
+    it('true is true', () => expect(true).toBe(true));
+  });
+});
+
+
