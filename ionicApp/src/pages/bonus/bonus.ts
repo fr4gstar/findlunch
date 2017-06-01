fb1528879ee8cd44f38fe912b9e6bb638adbd925
Sergej Bardin <sergej_bardin@yahoo.de>
2017-06-01 16:48:34 +0200

    Display Error-Messages on server response

--- ionicApp/src/pages/bonus/bonus.ts
+++ ionicApp/src/pages/bonus/bonus.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from "@angular/core";
-import {NavController, NavParams, ViewController} from "ionic-angular";
+import {NavController, NavParams, ToastController} from "ionic-angular";
 import {Headers, Http, RequestOptions, RequestMethod} from "@angular/http";
 import {OrderDetailsPage} from "../order-details/orderdetails";
 import {SERVER_URL} from "../../app/app.module";
@@ -17,9 +17,7 @@ export class BonusPage implements OnInit {
 
   public points: Object[];
 
-    constructor(private barcodeScanner: BarcodeScanner, navParams: NavParams, private http: Http, private navCtrl: NavController) {
-
-
+    constructor(private barcodeScanner: BarcodeScanner, private toastCtrl: ToastController, navParams: NavParams, private http: Http, private navCtrl: NavController) {
     }
 
     ngOnInit() {
@@ -44,7 +42,7 @@ export class BonusPage implements OnInit {
          )
     }
 
-    public onQRClicked(event){
+    onQRClicked(event) {
       let headers = new Headers({
         'Content-Type': 'application/json',
         "Authorization": "Basic aW9uaWNAaW9uaWMuY29tOiExMjM0NTY3OE5p"
@@ -54,24 +52,49 @@ export class BonusPage implements OnInit {
         headers: headers,
         method: RequestMethod.Put
       });
-
-      console.log("Barcode scan started");
       this.barcodeScanner.scan()
         .then((barcodeData) => {
-          console.log(barcodeData);
           this.http.get(`${SERVER_URL}/api/confirm_reservation/`+barcodeData.text, options)
             .subscribe(
-              res => console.log(res.json()),
-              err => console.error(err)
+              (res) => {
+                console.log("res: " + res.json());
+                let msg;
+                switch (res.json()){
+                  case 0:
+                    msg = "Bestellung erfolgreich bestätigt!"
+                    break;
+                  case 3:
+                    msg = "Fehler: Restaurant nicht gefunden!"
+                    break;
+                  case 4:
+                    msg = "Fehler: Offer nicht gefunden!"
+                    break;
+                }
+                console.log("Msg: " + msg);
+                const toast = this.toastCtrl.create({
+                  message: msg,
+                  duration: 3000
+                });
+                toast.present();
+              },
+              (err) => {
+                const toast = this.toastCtrl.create({
+                  message: "Fehler: Verbindung zum Server",
+                  duration: 3000
+                });
+                toast.present();
+              }
             )
-        // Success! Barcode data is here
         }, (err) => {
-        // An error occurred
+          const toast = this.toastCtrl.create({
+            message: "QR-Code Scan Fehler",
+            duration: 3000
+          });
+          toast.present();
         });
       }
 
-    showOffers(restaurant_id: String){
+    showOffers(restaurant_id: String) {
       this.navCtrl.push(OffersPage,{restaurant_id: restaurant_id});
     }
-
 }
