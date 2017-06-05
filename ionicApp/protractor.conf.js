ca4678b7e474357486b0d883634f96da85fdb42c
Sergej Bardin <sergej_bardin@yahoo.de>
2017-06-05 10:08:15 +0200

    testing chrome options

--- ionicApp/protractor.conf.js
+++ ionicApp/protractor.conf.js
@@ -9,7 +9,14 @@ exports.config = {
     './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    browserName: 'chrome',
+    chromeOptions: {
+      args: [
+        "--disable-gpu",
+        "--test-type",
+        "--no-sandbox"
+      ],
+    },
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
