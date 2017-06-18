a986948d5e4ecd45019ec546a18152a61ce0eb81
Sergej Bardin <sergej_bardin@yahoo.de>
2017-06-19 00:35:24 +0200

    Added simple version of password reset
    password reset via button at login view.
    ToDo:
    - Validation and/or count wrong login attempts
    - Check for design draft

--- ionicApp/src/pages/login/login.ts
+++ ionicApp/src/pages/login/login.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import {NavController, ToastController} from "ionic-angular";
 import {ModalController} from "ionic-angular";
-
+import {Headers, Http, RequestOptions, RequestMethod} from "@angular/http";
 import {HomePage} from "../home/home";
 import {RegistryPage} from "../registry/registry";
 import {AuthService} from "../../providers/auth-service";
-
+import {SERVER_URL} from "../../app/app.module";
 
 @Component({
   selector: 'login-page',
@@ -14,8 +14,13 @@ import {AuthService} from "../../providers/auth-service";
 })
 export class LoginPage {
 
-  constructor(private navCtrl: NavController, private toastCtrl: ToastController,
-              private auth: AuthService, private modCtrl: ModalController) { //TODO: Modalcontroller für Registerpage
+  constructor(private navCtrl: NavController,
+              private toastCtrl: ToastController,
+              private auth: AuthService,
+              private modCtrl: ModalController,
+              private http: Http) { //TODO: Modalcontroller für Registerpage
+
+
   }
 
 
@@ -35,5 +40,46 @@ export class LoginPage {
 
   public goToRegisterPage(){
     this.navCtrl.push(RegistryPage);
+  }
+
+  public isEmptyUser(userName) {
+    if(userName){
+      return false;
+    }
+    return true;
+  }
+
+  public sendPasswordReset(){
+    let user = window.localStorage.getItem("username");
+    let token = window.localStorage.getItem(user);
+    let headers = new Headers({
+      'Content-Type': 'application/json',
+      "Authorization": "Basic " +token
+    });
+
+    let options = new RequestOptions({
+      headers: headers,
+      method: RequestMethod.Post
+    });
+    this.http.get(`${SERVER_URL}/api/api/get_reset_token`, options)
+      .subscribe(
+        (res) => {
+          let msg;
+          switch (res.json()){
+            case 0:
+              msg = "Eine E-Mail mit der Passwortwiederherstellung wurde an Sie gesandt!"
+              break;
+            default:
+              msg = "Verbindungsfehler!"
+              break;
+          }
+          const toast = this.toastCtrl.create({
+            message: msg,
+            duration: 3000});
+          toast.present();
+        }, (err) => {
+          console.error(err)
+        }
+      )
   }
 }
