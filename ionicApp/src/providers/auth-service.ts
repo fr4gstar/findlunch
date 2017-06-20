aafa279a38580e4050f4c07bef1721c2e115720e
Skanny <smorandi@hm.edu>
2017-06-20 19:16:25 +0200

    update verifyUser only if last registered user not null

--- ionicApp/src/providers/auth-service.ts
+++ ionicApp/src/providers/auth-service.ts
@@ -78,25 +78,25 @@ public register(username: string, password: string) {
 
   public verifyUser() {
     //zuletzt eingeloggter user
-    let currentUser = window.localStorage.getItem("username");
-    let headers = new Headers({
-      'Content-Type': 'application/json',
-      //token zum zuletzt eingeloggten user, gespeichert als value zum key der Variable currentuser
-      "Authorization": "Basic " + window.localStorage.getItem(currentUser)
-    });
-    console.log("derzeitiger User " + currentUser);
-    console.log("vorhandener key :" + window.localStorage.getItem(currentUser));
-    let options = new RequestOptions({headers: headers});
-    return new Promise((resolve) => {
-      this.http.get(SERVER_URL + "/api/login_user", options).subscribe(
-        (res) => {
-          console.log("user verifiziert");
-          this.loggedIn = true;
-        }, (err) => {
-          console.log("user konnte nicht verifiziert werden \n automatischer Logout")
-          this.logout();
-        })
-    })
+    if(window.localStorage.getItem("username")!== null){
+
+      let currentUser = window.localStorage.getItem("username");
+      let headers = new Headers({
+        'Content-Type': 'application/json',
+        //token zum zuletzt eingeloggten user, gespeichert als value zum key der Variable currentuser
+        "Authorization": "Basic " + window.localStorage.getItem(currentUser)
+      });
+
+      let options = new RequestOptions({headers: headers});
+      return new Promise((resolve) => {
+        this.http.get(SERVER_URL + "/api/login_user", options).subscribe(
+          (res) => {
+            this.loggedIn = true;
+          }, (err) => {
+            this.logout();
+          })
+      })
+    }
   }
 
   public getLoggedIn(){
