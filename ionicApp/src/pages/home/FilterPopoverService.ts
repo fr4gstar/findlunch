acbd5dfd6010e9d0e4ad060682b9ed2058bfcd80
david <david.sautter@web.de>
2017-06-24 14:20:59 +0200

    implemented showing only favorite restaurants in map, not tested

--- ionicApp/src/pages/home/FilterPopoverService.ts
+++ ionicApp/src/pages/home/FilterPopoverService.ts
@@ -8,6 +8,7 @@ import "rxjs/Rx";
 export class FilterPopoverService {
   private _kitchenTypes: Array<KitchenType>;
   public selectedKitchenTypes: Array<KitchenType>;
+  public showOnlyFavorites: boolean;
 
   constructor(private http: Http) {
     this.http.get(SERVER_URL + "/api/kitchen_types").subscribe(res => {
@@ -19,12 +20,4 @@ export class FilterPopoverService {
   get kitchenTypes() {
     return this._kitchenTypes;
   }
-  /*get kitchenTypes() {
-    if (this._kitchenTypes) {
-      return Observable.create(() => {
-        return this._kitchenTypes;
-      })
-    }
-    return this.http.get(SERVER_URL + "/api/kitchen_types").map(res => this._kitchenTypes = res.json());
-  }*/
 }
