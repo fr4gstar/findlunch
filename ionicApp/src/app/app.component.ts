b30aee00176d5334310bc51b4df6c7594e950413
DASA <dasa@muc.isoware.com>
2017-06-30 17:35:04 +0200

    fixes #8068 Google maps view -> setClickable bei Push empfang

--- ionicApp/src/app/app.component.ts
+++ ionicApp/src/app/app.component.ts
@@ -1,16 +1,15 @@
 import {Component, ViewChild} from "@angular/core";
-import {Events, Nav, Platform} from "ionic-angular";
+import {AlertController, Events, Nav, Platform, ToastController} from "ionic-angular";
 import {StatusBar} from "@ionic-native/status-bar";
 import {SplashScreen} from "@ionic-native/splash-screen";
 import {SERVER_URL} from "../app/app.module";
-import {Headers, Http, RequestOptions, RequestMethod} from "@angular/http";
+import {Headers, Http, RequestMethod, RequestOptions} from "@angular/http";
 import {AuthService} from "../providers/auth-service";
 import {MenuService} from "../providers/menu-service";
-import {ToastController, AlertController} from "ionic-angular";
-import {Push, PushObject, PushOptions} from '@ionic-native/push';
+import {Push, PushObject, PushOptions} from "@ionic-native/push";
 import {QRService} from "../providers/QRService";
 import {InAppBrowser} from "@ionic-native/in-app-browser";
-import {HomePage} from '../pages/home/home';
+import {EVENT_TOPIC_MAP_CLICKABLE, HomePage} from "../pages/home/home";
 
 /**
  * Initialize the application.
@@ -18,150 +17,161 @@ import {HomePage} from '../pages/home/home';
  * 2. Sets the firebase-functionality of the application up.
  */
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
-  /**
-   * Sets the first site of the app
-   * @type {HomePage}
-   */
+    @ViewChild(Nav) nav: Nav;
+    /**
+     * Sets the first site of the app
+     * @type {HomePage}
+     */
     rootPage: any = HomePage;
 
-  pages: Array<{ title: string, component: any }>;
+    pages: Array<{ title: string, component: any }>;
 
-  /**
-   * Initialize modules.
-   * 1. Verify user
-   * 2. Push setup - firebase
-   *
-   * @param platform
-   * @param statusBar
-   * @param http
-   * @param splashScreen
-   * @param events
-   * @param auth
-   * @param menu
-   * @param toastCtrl
-   * @param push
-   * @param qr
-   * @param iab
-   * @param alertCtrl
-   */
-  constructor(public platform: Platform,
-              public statusBar: StatusBar,
-              private http: Http,
-              public splashScreen: SplashScreen,
-              private events: Events,
-              private auth: AuthService,
-              public menu: MenuService,
-              private toastCtrl: ToastController,
-              public push: Push,
-              public qr: QRService,
-              public iab: InAppBrowser,
-              public alertCtrl: AlertController) {
+    /**
+     * Initialize modules.
+     * 1. Verify user
+     * 2. Push setup - firebase
+     *
+     * @param platform
+     * @param statusBar
+     * @param http
+     * @param splashScreen
+     * @param events
+     * @param auth
+     * @param menu
+     * @param toastCtrl
+     * @param push
+     * @param qr
+     * @param iab
+     * @param alertCtrl
+     */
+    constructor(public platform: Platform,
+                public statusBar: StatusBar,
+                private http: Http,
+                public splashScreen: SplashScreen,
+                private events: Events,
+                private auth: AuthService,
+                public menu: MenuService,
+                private toastCtrl: ToastController,
+                public push: Push,
+                public qr: QRService,
+                public iab: InAppBrowser,
+                public alertCtrl: AlertController) {
 
-    this.auth.verifyUser();
-    this.pushSetup();
+        this.auth.verifyUser();
+        this.pushSetup();
 
-    //Listener, der bei "pausieren und wieder öffnen" der App loggedIn Status am Server verifiziert
-    document.addEventListener('resume', () => {
-    this.auth.verifyUser();
-    })
+        //Listener, der bei "pausieren und wieder öffnen" der App loggedIn Status am Server verifiziert
+        document.addEventListener('resume', () => {
+            this.auth.verifyUser();
+        })
 
-  }
+    }
 
-  /**
-   * Sets the firebase push configuration up.
-   * Register the device token at the backend.
-   * If the device receives a push message,
-   * it will be displayed as a notification.
-   */
-  pushSetup() {
-    let user = window.localStorage.getItem("username");
-    let token = window.localStorage.getItem(user);
-    let headers = new Headers({
-      'Content-Type': 'application/json',
-      "Authorization": "Basic " +token
-    });
+    /**
+     * Sets the firebase push configuration up.
+     * Register the device token at the backend.
+     * If the device receives a push message,
+     * it will be displayed as a notification.
+     */
+    pushSetup() {
+        let user = window.localStorage.getItem("username");
+        let token = window.localStorage.getItem(user);
+        let headers = new Headers({
+            'Content-Type': 'application/json',
+            "Authorization": "Basic " + token
+        });
 
-    let options = new RequestOptions({
-      headers: headers,
-      method: RequestMethod.Put
-    });
+        let options = new RequestOptions({
+            headers: headers,
+            method: RequestMethod.Put
+        });
 
-    const pushOptions: PushOptions = {
-      android: {
-        senderID: '343682752512',
-        icon: '',
-        vibrate: true
-      },
-      ios: {
-        alert: 'false',
-        badge: true,
-        sound: 'false'
-      },
-      windows: {}
-    };
+        const pushOptions: PushOptions = {
+            android: {
+                senderID: '343682752512',
+                icon: '',
+                vibrate: true
+            },
+            ios: {
+                alert: 'false',
+                badge: true,
+                sound: 'false'
+            },
+            windows: {}
+        };
 
-    const pushObject: PushObject = this.push.init(pushOptions);
+        const pushObject: PushObject = this.push.init(pushOptions);
 
-    pushObject.on('notification')
-      .subscribe((notification: any) => {
+        pushObject.on('notification')
+            .subscribe((notification: any) => {
 
-        // Foreground handling
-        if (notification.additionalData.foreground) {
-          let youralert = this.alertCtrl.create({
-            title: notification.title,
-            message: notification.message,
-            buttons: [{
-              text: 'Okay',
-              role: 'cancel'
-            }],
-          });
-          youralert.present();
-        }
-      });
+                // Foreground handling
+                if (notification.additionalData.foreground) {
+                    let youralert = this.alertCtrl.create({
+                        title: notification.title,
+                        message: notification.message,
+                        buttons: [{
+                            text: 'Okay',
+                            role: 'cancel',
+                            handler: () => {
+                                youralert.dismiss();
 
-    pushObject.on('registration')
-      .subscribe((registration: any) => {
-        this.http.get(`${SERVER_URL}/api/submitToken/${registration.registrationId}`, options)
-          .subscribe(
-            res => res,
-            err => console.error(err)
-          )
-      });
+                                // set map clickable again
+                                this.events.publish(EVENT_TOPIC_MAP_CLICKABLE, true);
+                            }
+                        }]
+                    });
 
-    pushObject.on('error').subscribe(error => console.log('Error with Push plugin' + error));
-  }
+                    // set map to be not clickable before alert shows
+                    this.events.publish(EVENT_TOPIC_MAP_CLICKABLE, false);
+
+                    youralert.present();
+                }
+            });
+
+        pushObject.on('registration')
+            .subscribe((registration: any) => {
+                this.http.get(`${SERVER_URL}/api/submitToken/${registration.registrationId}`, options)
+                    .subscribe(
+                        res => res,
+                        err => console.error(err)
+                    )
+            });
+
+        pushObject.on('error').subscribe(error => console.log('Error with Push plugin' + error));
+    }
 
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+    openPage(page) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.nav.setRoot(page.component);
+    }
 
-  public logout() {
-    this.auth.logout();
-    const toast = this.toastCtrl.create({
-      message: "Logout erfolgt",
-      duration: 3000
-    });
-    toast.present();
-  }
+    public logout() {
+        this.auth.logout();
+        const toast = this.toastCtrl.create({
+            message: "Logout erfolgt",
+            duration: 3000
+        });
+        toast.present();
+    }
 
-  onMenuClosed() {
-    this.events.publish("menu", "close");
-  }
+    onMenuClosed() {
+        this.events.publish(EVENT_TOPIC_MAP_CLICKABLE, true);
+    }
 
-  onMenuOpened() {
-    this.events.publish("menu", "open");
-  }
-   openUrl(url){
-    this.platform.ready().then(() => {
-      let browser = this.iab.create(url);
-    });
-  }
+    onMenuOpened() {
+        this.events.publish(EVENT_TOPIC_MAP_CLICKABLE, false);
+    }
+
+    openUrl(url) {
+        this.platform.ready().then(() => {
+            let browser = this.iab.create(url);
+        });
+    }
 
 }
--- ionicApp/src/pages/home/home.ts
+++ ionicApp/src/pages/home/home.ts
@@ -12,179 +12,179 @@ import {AddressInputComponent} from "./AddressInputComponent";
 
 export const ANDROID_API_KEY = "AIzaSyAvO9bl1Yi2hn7mkTSniv5lXaPRii1JxjI";
 export const CONFIG_GEOLOCATION_TIMEOUT = 2000;
+export const EVENT_TOPIC_MAP_CLICKABLE = "map:clickable";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  private _map: GoogleMap;
-  private _mapMarkers: Array<Marker> = [];
-  private _allRestaurants: Array<Restaurant>;
+    private _map: GoogleMap;
+    private _mapMarkers: Array<Marker> = [];
+    private _allRestaurants: Array<Restaurant>;
 
-  constructor(private navCtrl: NavController,
-              private geolocation: Geolocation,
-              private modalCtrl: ModalController,
-              private googleMaps: GoogleMaps,
-              private http: Http,
-              private popCtrl: PopoverController,
-              private popService: FilterPopoverService,
-              private events: Events,
-              private platform: Platform
-  ) {
-    this.events.subscribe("menu", eventData => {
-      if (eventData === "open"){
-        this._map.setClickable(false);
-      }
-      else if (eventData === "close") {
-        this._map.setClickable(true);
-      }
-    })
-      this.platform.ready().then(() => {
-          this.loadMap();
-      });
-  }
+    constructor(private navCtrl: NavController,
+                private geolocation: Geolocation,
+                private modalCtrl: ModalController,
+                private googleMaps: GoogleMaps,
+                private http: Http,
+                private popCtrl: PopoverController,
+                private popService: FilterPopoverService,
+                private events: Events,
+                private platform: Platform) {
+        this.events.subscribe(EVENT_TOPIC_MAP_CLICKABLE, eventData => {
+            if (eventData === false) {
+                this._map.setClickable(false);
+            }
+            else if (eventData === true) {
+                this._map.setClickable(true);
+            }
+        });
+        this.platform.ready().then(() => {
+            this.loadMap();
+        });
+    }
 
-  // Load map only after view is initialized
-  ngAfterViewInit() {
+    // Load map only after view is initialized
+    ngAfterViewInit() {
 
-  }
+    }
 
-  public openFilterDialog(ev: Event) {
-    this._map.setClickable(false);      // needed to be able to click on the overlay
+    public openFilterDialog(ev: Event) {
+        this._map.setClickable(false);      // needed to be able to click on the overlay
 
-    let pop = this.popCtrl.create(FilterPopoverComponent);
+        let pop = this.popCtrl.create(FilterPopoverComponent);
 
-    pop.present({ev});
+        pop.present({ev});
 
-    pop.onWillDismiss(() => {
-      // filter kitchen types
-      let newRestaurants = this._allRestaurants.filter(res => {
-        return res.kitchenTypes.some(resKitchenType => {
-          return this.popService.selectedKitchenTypes.some(selKitchenType => {
-            return resKitchenType.id === selKitchenType.id
-          })
+        pop.onWillDismiss(() => {
+            // filter kitchen types
+            let newRestaurants = this._allRestaurants.filter(res => {
+                return res.kitchenTypes.some(resKitchenType => {
+                    return this.popService.selectedKitchenTypes.some(selKitchenType => {
+                        return resKitchenType.id === selKitchenType.id
+                    })
+                })
+            });
+
+            // show only favorites
+            if (this.popService.showOnlyFavorites) {
+                newRestaurants = newRestaurants.filter(res => res.isFavorite);
+            }
+
+            this.setRestaurantMarkers(newRestaurants);
+            this._map.setClickable(true);
         })
-      });
-
-      // show only favorites
-      if (this.popService.showOnlyFavorites) {
-        newRestaurants = newRestaurants.filter(res => res.isFavorite);
-      }
-
-      this.setRestaurantMarkers(newRestaurants);
-      this._map.setClickable(true);
-    })
-  }
+    }
 
 
-  /**
-   * Initializes the Map and positions the current device on it.
-   */
-  private loadMap() {
-    // create a new map by passing HTMLElement
-    let element: HTMLElement = document.getElementById('map');
+    /**
+     * Initializes the Map and positions the current device on it.
+     */
+    private loadMap() {
+        // create a new map by passing HTMLElement
+        let element: HTMLElement = document.getElementById('map');
 
-    this._map = this.googleMaps.create(element);
+        this._map = this.googleMaps.create(element);
 
-    // listen to MAP_READY event
-    // You must wait for this event to fire before adding something to the map or modifying it in anyway
-    this._map.one(GoogleMapsEvent.MAP_READY).then(
-      () => {
-        console.log('Map is ready!');
-        // Now you can add elements to the map like the marker
+        // listen to MAP_READY event
+        // You must wait for this event to fire before adding something to the map or modifying it in anyway
+        this._map.one(GoogleMapsEvent.MAP_READY).then(
+            () => {
+                console.log('Map is ready!');
+                // Now you can add elements to the map like the marker
 
-        this._map.setMyLocationEnabled(true);
-        this._map.setAllGesturesEnabled(true);
-        this._map.setCompassEnabled(true);
+                this._map.setMyLocationEnabled(true);
+                this._map.setAllGesturesEnabled(true);
+                this._map.setCompassEnabled(true);
 
-        this.geolocation.getCurrentPosition({
-          timeout: CONFIG_GEOLOCATION_TIMEOUT
-        }).then((res) => {
-          let pos = new LatLng(res.coords.latitude, res.coords.longitude);
+                this.geolocation.getCurrentPosition({
+                    timeout: CONFIG_GEOLOCATION_TIMEOUT
+                }).then((res) => {
+                    let pos = new LatLng(res.coords.latitude, res.coords.longitude);
 
-          this.fetchRestaurants(res.coords);
+                    this.fetchRestaurants(res.coords);
 
-          let camPos: CameraPosition = {
-            target: pos,
-            zoom: 16
-          };
+                    let camPos: CameraPosition = {
+                        target: pos,
+                        zoom: 16
+                    };
 
-          this._map.moveCamera(camPos);
-        })
-            .catch(err => {
-              this._map.setMyLocationEnabled(false);
-              this.showAddressInput();
-            })
-      }
-    );
-  }
+                    this._map.moveCamera(camPos);
+                })
+                    .catch(err => {
+                        this._map.setMyLocationEnabled(false);
+                        this.showAddressInput();
+                    })
+            }
+        );
+    }
 
-  /**
-   * Fetches the restaurants from the server using the provided coordinates
-   * @param coords
-   */
-  private fetchRestaurants(coords: Coordinates) {
-    // do not filter by radius, because there are just a few restaurants.
-    // in the future it could filter by using the visible map-area.
-    this.http.get(`${SERVER_URL}/api/restaurants?latitude=${coords.latitude}&longitude=${coords.longitude}&radius=9999999`).subscribe(
-      res => {
-        this._allRestaurants = res.json();
-        this.setRestaurantMarkers(this._allRestaurants);
-      }
-    )
-  }
+    /**
+     * Fetches the restaurants from the server using the provided coordinates
+     * @param coords
+     */
+    private fetchRestaurants(coords: Coordinates) {
+        // do not filter by radius, because there are just a few restaurants.
+        // in the future it could filter by using the visible map-area.
+        this.http.get(`${SERVER_URL}/api/restaurants?latitude=${coords.latitude}&longitude=${coords.longitude}&radius=9999999`).subscribe(
+            res => {
+                this._allRestaurants = res.json();
+                this.setRestaurantMarkers(this._allRestaurants);
+            }
+        )
+    }
 
 
-  /**
-   *  Shows markers for the provided restaurants
-   */
-  private setRestaurantMarkers(restaurants) {
-    // remove old markers
-    this._mapMarkers.forEach(marker => {
-      marker.remove();
-    });
+    /**
+     *  Shows markers for the provided restaurants
+     */
+    private setRestaurantMarkers(restaurants) {
+        // remove old markers
+        this._mapMarkers.forEach(marker => {
+            marker.remove();
+        });
 
-    // draw new markers
-    restaurants.forEach((restaurant: Restaurant) => {
-      this._map.addMarker({
-        position: new LatLng(restaurant.locationLatitude, restaurant.locationLongitude),
-        icon: 'http://maps.google.com/mapfiles/kml/shapes/dining.png',
-        title: restaurant.name,
-        snippet: `Adresse: ${restaurant.street} ${restaurant.streetNumber}
+        // draw new markers
+        restaurants.forEach((restaurant: Restaurant) => {
+            this._map.addMarker({
+                position: new LatLng(restaurant.locationLatitude, restaurant.locationLongitude),
+                icon: 'http://maps.google.com/mapfiles/kml/shapes/dining.png',
+                title: restaurant.name,
+                snippet: `Adresse: ${restaurant.street} ${restaurant.streetNumber}
 Telefon: ${restaurant.phone}
 Küche: ${restaurant.kitchenTypes.map(type => type.name).join(', ')}
 Entfernung: ${restaurant.distance}m
 Jetzt geöffnet`,
 
-        infoClick: () => {
-          this.navCtrl.push(OffersPage,{restaurant: restaurant});
-        }
-      }).then(marker => {
-        this._mapMarkers.push(marker);
-      })
-    });
-  }
+                infoClick: () => {
+                    this.navCtrl.push(OffersPage, {restaurant: restaurant});
+                }
+            }).then(marker => {
+                this._mapMarkers.push(marker);
+            })
+        });
+    }
 
 
-  private showAddressInput() {
-    this._map.setClickable(false);
-    const modal = this.modalCtrl.create(AddressInputComponent);
-    modal.onWillDismiss(coords => {
-      const latlng = new LatLng(coords.latitude, coords.longitude);
-      this._map.setClickable(true);
-      this._map.addMarker({
-        position: latlng,
-        title: "Ihr Standort"
-      });
-      this._map.moveCamera({
-        target: latlng,
-        zoom: 16
-      });
+    private showAddressInput() {
+        this._map.setClickable(false);
+        const modal = this.modalCtrl.create(AddressInputComponent);
+        modal.onWillDismiss(coords => {
+            const latlng = new LatLng(coords.latitude, coords.longitude);
+            this._map.setClickable(true);
+            this._map.addMarker({
+                position: latlng,
+                title: "Ihr Standort"
+            });
+            this._map.moveCamera({
+                target: latlng,
+                zoom: 16
+            });
 
-      this.fetchRestaurants(coords)
-    });
-    modal.present();
-  }
+            this.fetchRestaurants(coords)
+        });
+        modal.present();
+    }
 }
