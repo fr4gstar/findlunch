9b8b5d6aecd078290f30639b1003d5f075b9fd7a
Skanny <smorandi@hm.edu>
2017-07-03 08:25:17 +0200

    "Meine Bestellungen"  ordered by restaurant

--- ionicApp/src/pages/reservations/reservations.ts
+++ ionicApp/src/pages/reservations/reservations.ts
@@ -9,52 +9,72 @@ import {ReservationViewPage} from "../reservation-view/reservation-view";
  * @author Sergej Bardin - bardin@hm.edu
  */
 @Component({
-  selector: 'reservations-page',
-  templateUrl: 'reservations.html'
+    selector: 'reservations-page',
+    templateUrl: 'reservations.html'
 })
 export class ReservationsPage implements OnInit {
-  public reservations;
+    public reservations;
+    public usedRestaurants: String[];
 
-  /**
-   * Initialize modules
-   * @param navCtrl
-   * @param http
-   */
-  constructor(
-    public navCtrl : NavController,
-    private http: Http) {
-  }
 
-  /**
-   * Loads the reservation of an user.
-   */
-  ngOnInit() {
-    let user = window.localStorage.getItem("username");
-    let token = window.localStorage.getItem(user);
-    let headers = new Headers({
-      'Content-Type': 'application/json',
-      "Authorization": "Basic " +token
-    });
+    /**
+     * Initialize modules
+     * @param navCtrl
+     * @param http
+     */
+    constructor(public navCtrl: NavController,
+                private http: Http) {
 
-    let options = new RequestOptions({
-      headers: headers,
-      method: RequestMethod.Get
-    });
+        this.usedRestaurants = [];
 
-    this.http.get(`${SERVER_URL}/api/getCustomerReservations`, options)
-      .subscribe(
-        res =>
-          this.reservations = res.json(),
-        err => console.error(err)
+    }
 
-      );
-   }
-  /**
-   * Opens a reservation detail view on click.
-   * @param event
-   * @param reservation
-   */
- public onReservationClicked(event, reservation: String){
-    this.navCtrl.push(ReservationViewPage,{reservation: reservation});
- }
+    /**
+     * Loads the reservation of an user.
+     */
+    ngOnInit() {
+        let user = window.localStorage.getItem("username");
+        let token = window.localStorage.getItem(user);
+        let headers = new Headers({
+            'Content-Type': 'application/json',
+            "Authorization": "Basic " + token
+        });
+
+        let options = new RequestOptions({
+            headers: headers,
+            method: RequestMethod.Get
+        });
+
+        this.http.get(`${SERVER_URL}/api/getCustomerReservations`, options)
+            .subscribe(
+                res => {
+
+                    this.reservations = res.json();
+                    if(this.reservations.length > 0){
+                        this.collectUsedRestaurants();
+                     }
+
+
+                },
+                err => console.error(err)
+            );
+    }
+
+    public collectUsedRestaurants() {
+        for (let reservation of this.reservations) {
+            if (this.usedRestaurants.indexOf(reservation.restaurant.name) === -1) {
+                this.usedRestaurants.push(reservation.restaurant.name);
+            }
+        }
+
+    }
+
+    /**
+     * Opens a reservation detail view on click.
+     * @param event
+     * @param reservation
+     */
+    public onReservationClicked(event, reservation: String) {
+        this.navCtrl.push(ReservationViewPage, {reservation: reservation});
+    }
 }
