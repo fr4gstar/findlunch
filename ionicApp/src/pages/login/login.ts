30fcef1e31a54ce175afe56ddfaaa188fd251c32
Skanny <smorandi@hm.edu>
2017-07-16 10:58:07 +0200

    fixed loader for comback from warenkorb

--- ionicApp/src/pages/login/login.ts
+++ ionicApp/src/pages/login/login.ts
@@ -9,111 +9,114 @@ import {LoadingService} from "../../providers/loading-service";
 
 
 @Component({
-  selector: 'login-page',
-  templateUrl: 'login.html'
+    selector: 'login-page',
+    templateUrl: 'login.html'
 
 })
 export class LoginPage {
 
-  popThisPage : boolean;
-  private counterPasswordWrong: number = 0;
+    popThisPage: boolean;
+    private counterPasswordWrong: number = 0;
 
-      constructor(private navCtrl: NavController,
-                  private toastCtrl: ToastController,
-                  private auth: AuthService,
-                  private http: Http,
-                  navParams: NavParams,
-                  private loading: LoadingService) {
+    constructor(private navCtrl: NavController,
+                private toastCtrl: ToastController,
+                private auth: AuthService,
+                private http: Http,
+                navParams: NavParams,
+                private loading: LoadingService) {
 
-    this.popThisPage = navParams.get("comeBack");
-  }
+        this.popThisPage = navParams.get("comeBack");
+    }
 
 
-  public login(userName: string, password: string) {
-    this.counterPasswordWrong ++;
-    let loader = this.loading.prepareLoader("Einloggen...");
-    loader.present().then(res => {
+    public login(userName: string, password: string) {
+        this.counterPasswordWrong++;
+        let loader = this.loading.prepareLoader("Einloggen...");
+        loader.present().then(res => {
 
-        this.auth.login(userName, password).then(data => {
-          if (data) {
+            this.auth.login(userName, password).then(data => {
+                if (data) {
 
-            const toast = this.toastCtrl.create({
-              message: "Login Erfolgreich",
-              duration: 3000
-            });
-            toast.present();
+                    const toast = this.toastCtrl.create({
+                        message: "Login Erfolgreich",
+                        duration: 3000
+                    });
+                    toast.present();
 
-            if(this.popThisPage){
-              this.navCtrl.pop();
-            }else {
-              this.navCtrl.setRoot(HomePage);
-              loader.dismiss();
-            }
-          } else{
-            loader.dismiss();
-            alert("E-Mail und/oder Passwort nicht bekannt");
-          }
+                    if (this.popThisPage) {
+                        this.navCtrl.pop();
+                        loader.dismiss();
+
+                    } else {
+                        this.navCtrl.setRoot(HomePage);
+                        loader.dismiss();
+                    }
+                } else {
+                    loader.dismiss();
+                    alert("E-Mail und/oder Passwort nicht bekannt");
+                }
+            })
         })
-    })
 
-  }
+    }
 
-  public goToRegisterPage() {
-    this.navCtrl.push(RegistryPage);
-  }
+    public goToRegisterPage() {
+        this.navCtrl.push(RegistryPage);
+    }
 
-  /**
-   * Checks the input username
-   * @param username = email adress of user
-   */
-  public isEmptyUser(username) {
-        if(username && this.counterPasswordWrong >=1){
+    /**
+     * Checks the input username
+     * @param username = email adress of user
+     */
+    public isEmptyUser(username) {
+        if (username && this.counterPasswordWrong >= 1) {
             return false;
-          }
-        return true;
-      };
-
-  /**
-   * Requesting a password reset by the backend
-   * @param username = email adress of user
-   */
-  public sendPasswordReset(username){
-    let headers = new Headers({
-      'Content-Type': 'application/json'
-    });
-
-    let user = {
-        user: {
-            username: username
         }
+        return true;
     };
 
-    let options = new RequestOptions({
-      headers: headers,
-      method: RequestMethod.Post,
-      body: JSON.stringify(user)
-    });
+    /**
+     * Requesting a password reset by the backend
+     * @param username = email adress of user
+     */
+    public sendPasswordReset(username) {
+        let headers = new Headers({
+            'Content-Type': 'application/json'
+        });
 
-    this.http.get(`${SERVER_URL}/api/get_reset_token`, options)
-      .subscribe(
-        (res) => {
-          let msg;
-          switch (res.json()){
-            case 0:
-              msg = "Eine E-Mail mit der Passwortwiederherstellung wurde an Sie gesandt!"
-              break;
-            default:
-              msg = "Verbindungsfehler!"
-              break;
-          }
-          const toast = this.toastCtrl.create({
-            message: msg,
-            duration: 3000});
-          toast.present();
-        }, (err) => {
-          console.error(err)
-        }
-      )
-  }
+        let user = {
+            user: {
+                username: username
+            }
+        };
+
+        let options = new RequestOptions({
+            headers: headers,
+            method: RequestMethod.Post,
+            body: JSON.stringify(user)
+        });
+
+        this.http.get(`${SERVER_URL}/api/get_reset_token`, options)
+            .subscribe(
+                (res) => {
+                    let msg;
+                    switch (res.json()) {
+                        case 0:
+                            msg = "Eine E-Mail mit der Passwortwiederherstellung wurde an Sie gesandt!"
+                            break;
+                        default:
+                            msg = "Verbindungsfehler!"
+                            break;
+                    }
+                    const toast = this.toastCtrl.create({
+                        message: msg,
+                        duration: 3000
+                    });
+                    toast.present();
+                }, (err) => {
+                    console.error(err)
+                }
+            )
+    }
 
 }
